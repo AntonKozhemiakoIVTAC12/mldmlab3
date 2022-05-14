3e57493501366b5ae31aa8ac10b3d364d347291f
AntonKozhemiakoIVTAC12 <98730634+AntonKozhemiakoIVTAC12@users.noreply.github.com>
2022-05-14 17:41:34 +0400

    Add files via upload

---
+++ лабы.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name = "viewport" content ="witdh=device-width">
+    <link rel="stylesheet" href="styles/styles.css">
+    <script type="text/javascript" src="scripts/scripts.js"></script>
+</head>
+<div class = "conatainer">
+<body>
+<div class="panelnav">
+        <a href="index.php" class="double-border-button"><font size=4>Главная</font></a></td>
+        <a href="лабы.php" class="double-border-button"><font size=4>Лабы по дискретке</font></a></td>
+        <a href="obomne.php" class="double-border-button"><font size=4>О себе</font></a></td>
+        <a href="game.php" class="double-border-button"><font size=4>Моя любимая игра</font></a></td>
+        <a href="mydog.php" class="double-border-button"><font size=4>Моя собака</font></a></td>
+        <a href="gallery.php" class="double-border-button"><font size=4>Галерея</font></a></td>
+        <div class="avtorisator">
+            <a href="enter.php" class="double-border-button"><font size=4>Вход</font></a></td>
+            <a href="registration.php" class="double-border-button"><font size=4>Регистрация</font></a></td>
+        </div>
+</div>
+<div class = "menu">
+    <a href="menu.php" class="double-border-button"><font size=4>Меню</font></a></td>
+</div>
+<h1 class=mylabs> Мои лабораторнаые работы </h1>
+<div class=labs>
+    <a href="mldmlab1.php">Первая лабораторная работа по МЛидМ</a> <br>
+    <a href="mldmlab2.php">Вторая лабораторная работа по МЛидМ</a> <br>
+    <a href="mldmlab3.php">Третья лабораторная работа по МЛидМ</a> <br>
+</div>
+</body>
+</html>
+</div>
+</div>
